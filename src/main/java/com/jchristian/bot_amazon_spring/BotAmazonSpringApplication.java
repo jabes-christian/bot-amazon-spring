4462a049fad6a77e5ef1e06a24f361b563a6f4dd
jabes-christian <jabescristian08@gmail.com>
2026-09-02 22:03:43 -0300

    chore: estrutura inicial do projeto via Spring Initializr

---
+++ src/main/java/com/jchristian/bot_amazon_spring/BotAmazonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.jchristian.bot_amazon_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotAmazonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotAmazonSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jchristian/bot_amazon_spring/BotAmazonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jchristian.bot_amazon_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotAmazonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
